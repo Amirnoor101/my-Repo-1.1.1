24161e88366d4b6b65c8aaea69228001548a2b5e
Amir Noor <gitlab.amirnoor@gmail.com>
2023-11-17 09:57:03 +0330

    used render and templates

--- website/views.py
+++ website/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse as res
 
+
+
+# Funcs
+
 def index_views (request) :
-    return res ('<h1>Home page<h1\>')
+    return render (request,'index.html')
+
 def about_views (request) :
-    return res ('<h1>I124Q<h1\>')
+    return render (request,'about.html')
+
 def contact_views (request) :
-    return res ('<h1>Crazy<h1\>')
+    return render (request,'contact.html')
